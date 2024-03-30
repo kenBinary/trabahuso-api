11ad5706ac44a451c3f50b8eee31ef3e88a93356
thennek0 <kennethbecaro0@gmail.com>
2024-03-30 17:00:14 +0800

    refactor: change lib to better-sqlite3

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -1,13 +1,6 @@
 require("dotenv").config();
 
-const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database(process.env.SQLITE_DB_PATH, (err) => {
-    if (err == null) {
-        console.log("database opened successfully");
-    } else {
-        console.log(err);
-    }
-});
+const db = require("better-sqlite3")(process.env.SQLITE_DB_PATH, { fileMustExist: true });
+db.pragma("journal_mode = WAL");
 
 module.exports = db;
