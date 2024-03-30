07a2a15e13190f7db22a73ff04c7ab4f3d68dc01
thennek0 <kennethbecaro0@gmail.com>
2024-03-30 20:36:15 +0800

    feat: functions to retrieve job data

---
+++ controllers/jobsController.js
@@ -0,0 +1,53 @@
+const asyncHandler = require("express-async-handler");
+const db = require("../helpers/dbConnection");
+
+exports.getJobs = asyncHandler(async (req, res) => {
+  try {
+    const months = {
+      jan: 1,
+      feb: 2,
+      mar: 3,
+      apr: 4,
+      may: 5,
+      june: 6,
+      july: 7,
+      aug: 8,
+      sept: 9,
+      oct: 10,
+      nov: 11,
+      dec: 12,
+    };
+    const { month, year, province } = req.query;
+
+    const statement = db.prepare(
+      "SELECT job_title, location, salary, job_level, date_scraped FROM job_data"
+    );
+    let jobs = statement.all();
+
+    if (year) {
+      jobs = jobs.filter((job) => {
+        const jobYear = Number(job["date_scraped"].split("-")[0]);
+        console.log(jobYear);
+        return Number(year) === jobYear;
+      });
+    }
+
+    if (month) {
+      jobs = jobs.filter((job) => {
+        const jobMonth = Number(job["date_scraped"].split("-")[1]);
+        return months[month] === jobMonth;
+      });
+    }
+
+    if (province) {
+      jobs = jobs.filter((job) => {
+        const jobLocation = job["location"].split(",")[0];
+        return province.toUpperCase() === jobLocation.toUpperCase();
+      });
+    }
+
+    res.status(200).send(jobs);
+  } catch (error) {
+    res.status(400).send("error occured while retrieving jobs");
+  }
+});
