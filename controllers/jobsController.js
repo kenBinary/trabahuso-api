70a02e04e48c5dacbeade4d7b740e4c9d9e29ab9
thennek0 <kennethbecaro0@gmail.com>
2024-03-31 22:59:32 +0800

    feat: controller to retrieve job salaries

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../helpers/dbConnection");
+const stat = require("../utils/statUtils");
 
 exports.getJobs = asyncHandler(async (req, res) => {
   try {
@@ -144,6 +145,51 @@ exports.getJobByProvince = asyncHandler(async (req, res) => {
 
     res.status(200).json(jobDetail);
   } catch (error) {
+    res.status(400).send("error");
+  }
+});
+
+exports.getSalary = asyncHandler(async (req, res) => {
+  try {
+    let salaryDetails = {
+      undisclosed: null,
+      disclosed: null,
+    };
+
+    const salaryListQuery = db.prepare(
+      "SELECT salary FROM job_data where salary is not null"
+    );
+    salaryListQuery.raw();
+    const salaryList = salaryListQuery
+      .all()
+      .flat()
+      .sort((a, b) => a - b);
+
+    const nullSalaryListQuery = db.prepare(
+      "SELECT salary FROM job_data where salary is null"
+    );
+    nullSalaryListQuery.raw();
+    const nullSalaryList = nullSalaryListQuery.all().flat();
+
+    let frequencyDistribution = stat.getFrequencyDistribution(salaryList);
+
+    for (const salaryRange in frequencyDistribution) {
+      const [min, max] = salaryRange.split("-");
+      let rangeCount = 0;
+      salaryList.forEach((salary) => {
+        if (salary >= Number(min) && salary <= max) {
+          rangeCount += 1;
+        }
+      });
+      frequencyDistribution[salaryRange] = rangeCount;
+    }
+
+    salaryDetails.undisclosed = nullSalaryList.length;
+    salaryDetails.disclosed = frequencyDistribution;
+
+    res.status(200).json(salaryDetails);
+  } catch (error) {
+    console.error(error);
     res.status(400).send("error");
   }
 });
