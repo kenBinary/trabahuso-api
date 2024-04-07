771a3f9364f26526cdde8b7f9399279f01e2c6b4
thennek0 <kennethbecaro0@gmail.com>
2024-04-08 00:29:36 +0800

    feat: app setup

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-let createError = require("http-errors");
-let express = require("express");
-let path = require("path");
-let cookieParser = require("cookie-parser");
-let logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const helmet = require("helmet");
 
-let indexRouter = require("./routes/index");
-let usersRouter = require("./routes/users");
+const jobsRouter = require("./routes/jobs");
+const locationsRouter = require("./routes/locations");
+const salaryRouter = require("./routes/salary");
+const techStackRouter = require("./routes/techStack");
 
-let app = express();
+const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/jobs", jobsRouter);
+app.use("/api/job-locations", locationsRouter);
+app.use("/api/job-salaries", salaryRouter);
+app.use("/api/tech-stack", techStackRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+// eslint-disable-next-line no-unused-vars
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).send("an error occured");
 });
 
 module.exports = app;
