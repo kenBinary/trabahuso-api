ce4209ef7927c2866a234109d06d274c6b4e6ed0
kenBinary <joshuabecaro@gmail.com>
2024-11-16 23:30:05 +0800

    feat: add job controller

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using trabahuso_api.DTOs.Querries;
+using trabahuso_api.Interfaces;
 
 namespace trabahuso_api.Controllers
 {
-    public class JobController
+    [ApiController]
+    [Route("api/jobs")]
+    public class JobController : ControllerBase
     {
+        private readonly IJobRepository _jobRepository;
+        public JobController(IJobRepository jobRepository)
+        {
+            _jobRepository = jobRepository;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetJobs([FromQuery] QueryObject queryParams)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var jobs = await _jobRepository.GetAllAsync(queryParams);
+
+            return Ok(jobs);
+        }
     }
 }
