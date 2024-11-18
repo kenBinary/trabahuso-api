69be3768641ba80de4f83f26b0dd78eb50600a66
kenBinary <joshuabecaro@gmail.com>
2024-11-18 16:30:22 +0800

    feat: setup services

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using trabahuso_api.Helpers;
+using trabahuso_api.Interfaces;
+using trabahuso_api.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.AddScoped<ISqliteQueryCompiler, SqliteQueryCompiler>();
 
 var app = builder.Build();
 
@@ -13,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
