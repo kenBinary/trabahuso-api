55180bf583bc53cf27463998259a86773254a410
kenBinary <joshuabecaro@gmail.com>
2024-11-22 01:36:05 +0800

    feat: add individual quer filters for different resources

--- DTOs/Querries/QueryFilters.cs
+++ DTOs/Querries/QueryFilters.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using trabahuso_api.CustomAttributes;
 
@@ -9,9 +10,6 @@ namespace trabahuso_api.DTOs.Querries
 {
     public record QueryFilters
     {
-        [AllowedValuesCustom(["job_data_id", "job_title", "location", "salary", "job_level", "date_scraped"])]
-        public string? SortBy { get; set; }
-        public bool IsDescending { get; set; } = false;
         public int PageNumber { get; set; } = 0;
         [Range(0, 25, ErrorMessage = "Page size exceeded, it must be between 0 and 25")]
         public int PageSize { get; set; } = 25;
@@ -19,6 +17,18 @@ namespace trabahuso_api.DTOs.Querries
 
         // TODO: implement filtering of fields
         // public string[] Includes { get; set; } = [];
+    }
+
+    public record TechSkillFilterse : QueryFilters
+    {
+
+    }
+
+    public record JobFilters : QueryFilters
+    {
+        [AllowedValuesCustom(["job_data_id", "job_title", "location", "salary", "job_level", "date_scraped"])]
+        public string? SortBy { get; set; }
+        public bool IsDescending { get; set; } = false;
     }
 
 }
