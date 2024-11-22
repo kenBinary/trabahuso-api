0307b1c324bbfb758604055a514182ba5dc21bd7
kenBinary <joshuabecaro@gmail.com>
2024-11-22 23:04:08 +0800

    refactor: removed json data annotations

--- Models/TechSkill.cs
+++ Models/TechSkill.cs
@@ -8,11 +8,8 @@ namespace trabahuso_api.Models
 {
     public class TechSkill
     {
-        [JsonPropertyName("tech_stack_id")]
         public required string TechStackId { get; set; }
-        [JsonPropertyName("job_data_id")]
         public required string JobDataId { get; set; }
-        [JsonPropertyName("tech_type")]
         public required string TechType { get; set; }
     }
 }
