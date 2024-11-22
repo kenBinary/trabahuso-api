e7a08e544c9e7924a6993887b8c8fe91b920efd5
kenBinary <joshuabecaro@gmail.com>
2024-11-22 23:02:24 +0800

    feat: add tech skill DTO

---
+++ DTOs/TechSkill/TechSkillDto.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trabahuso_api.DTOs.TechSkill
+{
+    public record TechSkillDto(string TechStackId, string TechType);
+}
